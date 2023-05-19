3ab5af4433e638e4368a6b7b8d48bfcceeec901f
Logan Scheid <scheid.logan@gmail.com>
2023-05-19 12:21:17 -0400

    fixed toggle functionality

--- wp-content/themes/henp/template-parts/modules/counties-map/assets/js/counties-map.js
+++ wp-content/themes/henp/template-parts/modules/counties-map/assets/js/counties-map.js
@@ -4,51 +4,55 @@ svg.addEventListener('click', (event) => {
   if (g) {
     const gId = g.getAttribute('id');
     const toggleId = gId + '-toggle';
-    // Get the toggle element by ID
     const toggleElement = document.getElementById(toggleId);
     const triggerElement = toggleElement.querySelector('.toggle__trigger');
 
-    if (triggerElement.classList.contains('toggle--active')) {
-      // Remove the active class
-      triggerElement.classList.remove('toggle--active');
+    const isActive = triggerElement.classList.contains('toggle--active');
 
-      // Set ARIA attributes
-      toggleElement.setAttribute('aria-expanded', 'false');
-      const toggleBox = toggleElement.querySelector('.toggle__box');
-      if (toggleBox) {
-        toggleBox.setAttribute('aria-hidden', 'true');
+    const allToggles = document.querySelectorAll('.toggle');
+    allToggles.forEach(function (toggle) {
+      const toggleTrigger = toggle.querySelector('.toggle__trigger');
+      const toggleBox = toggle.querySelector('.toggle__box');
+      if (toggle !== toggleElement) {
+        toggle.classList.remove('toggle--active');
+        toggleTrigger.setAttribute('aria-expanded', 'false');
+        toggle.setAttribute('aria-expanded', 'false');
+        if (toggleBox) {
+          toggleBox.setAttribute('aria-hidden', 'true');
+          toggleBox.style.display = 'none';
+        }
+        const triggerText = toggle.querySelector('.toggle__trigger-text');
+        if (triggerText) {
+          triggerText.textContent = triggerText.dataset.show;
+        }
       }
+    });
 
-      // Change the toggle trigger text
-      const triggerText = toggleElement.querySelector('.toggle__trigger-text');
-      if (triggerText) {
-        triggerText.textContent = triggerText.dataset.show;
-      }
-
-      // Hide the toggle content box
-      if (toggleBox) {
-        toggleBox.style.display = 'none';
-      }
-    } else {
-      // Set the toggle as active
-      triggerElement.classList.add('toggle--active');
-
-      // Set ARIA attributes
+    if (!isActive) {
+      toggleElement.classList.add('toggle--active');
+      triggerElement.setAttribute('aria-expanded', 'true');
       toggleElement.setAttribute('aria-expanded', 'true');
       const toggleBox = toggleElement.querySelector('.toggle__box');
       if (toggleBox) {
         toggleBox.setAttribute('aria-hidden', 'false');
+        toggleBox.style.display = 'block';
       }
-
-      // Change the toggle trigger text
       const triggerText = toggleElement.querySelector('.toggle__trigger-text');
       if (triggerText) {
         triggerText.textContent = triggerText.dataset.hide;
       }
-
-      // Show the toggle content box
+    } else {
+      toggleElement.classList.remove('toggle--active');
+      triggerElement.setAttribute('aria-expanded', 'false');
+      toggleElement.setAttribute('aria-expanded', 'false');
+      const toggleBox = toggleElement.querySelector('.toggle__box');
       if (toggleBox) {
-        toggleBox.style.display = 'block';
+        toggleBox.setAttribute('aria-hidden', 'true');
+        toggleBox.style.display = 'none';
+      }
+      const triggerText = toggleElement.querySelector('.toggle__trigger-text');
+      if (triggerText) {
+        triggerText.textContent = triggerText.dataset.show;
       }
     }
   }
--- wp-content/themes/henp/template-parts/modules/toggles/assets/js/toggles.js
+++ wp-content/themes/henp/template-parts/modules/toggles/assets/js/toggles.js
@@ -1,77 +1,74 @@
 (function () {
-    const toggles = document.querySelectorAll('.toggle');
-    if (typeof toggles !== 'undefined' && toggles !== null) {
-      Toggles(toggles);
-    }
-  })();
-  
-  /**
-   * Default toggle functionality
-   *
-   * @param {Node} toggles Node list of all toggles
-   */
-  function Toggles(toggles) {
-    function toggleBehavior(e) {
-      e.preventDefault();
-  
-      const $this = e.target;
-      const toggleBox = $this.nextElementSibling;
-  
-      // Close all other open toggles
-      const allToggles = Array.from(toggles);
-      allToggles.forEach(function (toggle) {
-        const trigger = toggle.querySelector('.toggle__trigger');
-        if (trigger !== $this && trigger.getAttribute('aria-expanded') === 'true') {
-          trigger.setAttribute('aria-expanded', 'false');
-          toggle.querySelector('.toggle__box').setAttribute('aria-hidden', 'true');
-          toggle.classList.remove('toggle--active');
-        }
-      });
-  
-      // Control ARIA landmarks on open
-      if ($this.getAttribute('aria-expanded') === 'false') {
-        $this.setAttribute('aria-expanded', 'true');
-        toggleBox.setAttribute('aria-hidden', 'false');
-      } else {
-        // Control ARIA landmarks on close
-        $this.setAttribute('aria-expanded', 'false');
-        toggleBox.setAttribute('aria-hidden', 'true');
-      }
-  
-      // Check if toggle trigger has multiple children
-      let triggerText = $this.querySelector('.toggle__trigger-text');
-      if (triggerText !== null) {
-        const revealedText = triggerText.dataset.show;
-        const hiddenText = triggerText.dataset.hide;
-  
-        // Only run if data attributes found
-        if (typeof revealedText !== 'undefined' && typeof hiddenText !== 'undefined') {
-          // Change value based on if toggle is active
-          const toggleText = $this.classList.contains('toggle--active') ? revealedText : hiddenText;
-  
-          // Replace the current text value
-          triggerText.textContent = toggleText;
-        }
-      }
-  
-      $this.classList.toggle('toggle--active');
-  
-      const toggleBoxDisplay = $this.classList.contains('toggle--active') ? 'block' : 'none';
-      toggleBox.style.display = toggleBoxDisplay;
-    }
-  
-    Array.prototype.slice.call(toggles, 0).forEach(function (e) {
-      const toggleBox = e.querySelector('.toggle__box');
-      const trigger = e.querySelector('.toggle__trigger');
-  
-      // Check to make sure box is intended to be hidden on load
-      if (toggleBox.getAttribute('aria-hidden') !== 'false' || trigger.getAttribute('aria-expanded') === 'false') {
-        toggleBox.style.display = 'none';
-      } else {
-        toggleBox.style.display = 'block';
-      }
-  
-      trigger.addEventListener('click', toggleBehavior);
-    });
+  const toggles = document.querySelectorAll('.toggle');
+  if (typeof toggles !== 'undefined' && toggles !== null) {
+    Toggles(toggles);
   }
-  
+})();
+
+/**
+ * Default toggle functionality
+ *
+ * @param {Node} toggles Node list of all toggles
+ */
+function Toggles(toggles) {
+  function toggleBehavior(e) {
+    e.preventDefault();
+
+    const $this = e.target.closest('.toggle');
+    const toggleBox = $this.querySelector('.toggle__box');
+
+    const isActive = $this.classList.contains('toggle--active');
+
+    // Close all other toggles before opening the current one
+    Array.from(toggles).forEach(function (toggle) {
+      if (toggle !== $this && toggle.classList.contains('toggle--active')) {
+        toggle.classList.remove('toggle--active');
+        toggle.setAttribute('aria-expanded', 'false');
+        toggle.querySelector('.toggle__box').setAttribute('aria-hidden', 'true');
+        toggle.querySelector('.toggle__box').style.display = 'none';
+      }
+    });
+
+    // Control ARIA landmarks on open
+    if (!isActive) {
+      $this.setAttribute('aria-expanded', 'true');
+      toggleBox.setAttribute('aria-hidden', 'false');
+    } else {
+      // Control ARIA landmarks on close
+      $this.setAttribute('aria-expanded', 'false');
+      toggleBox.setAttribute('aria-hidden', 'true');
+    }
+
+    // Check if toggle trigger has multiple children
+    let triggerText = $this.querySelector('.toggle__trigger-text');
+    if (triggerText !== null) {
+      const revealedText = triggerText.dataset.show;
+      const hiddenText = triggerText.dataset.hide;
+
+      // Only run if data attributes found
+      if (typeof revealedText !== 'undefined' && typeof hiddenText !== 'undefined') {
+        // Change value based on if toggle is active
+        const toggleText = $this.classList.contains('toggle--active') ? revealedText : hiddenText;
+
+        // Replace the current text value
+        triggerText.textContent = toggleText;
+      }
+    }
+
+    $this.classList.toggle('toggle--active');
+
+    const toggleBoxDisplay = $this.classList.contains('toggle--active') ? 'block' : 'none';
+    toggleBox.style.display = toggleBoxDisplay;
+  }
+
+  Array.prototype.slice.call(toggles, 0).forEach(function (e) {
+    const toggleBox = e.querySelector('.toggle__box');
+
+    // Check to make sure box is intended to be hidden on load
+    if (toggleBox.getAttribute('aria-hidden') !== 'false') {
+      toggleBox.style.display = 'none';
+    }
+
+    e.querySelector('.toggle__trigger').addEventListener('click', toggleBehavior);
+  });
+}
